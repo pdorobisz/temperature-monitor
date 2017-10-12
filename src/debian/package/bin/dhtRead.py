1f6421410b99679df71c3b37d170f83c097df84c
Piotr Dorobisz <pdorobisz@gmail.com>
2017-10-12 10:06:02 +0100

    return empty string when sensor not available

--- src/debian/package/bin/dhtRead.py
+++ src/debian/package/bin/dhtRead.py
@@ -3,4 +3,8 @@
 import Adafruit_DHT as dht
 
 humidity, temperature =  dht.read_retry(dht.DHT22, 22)
-print('{0:0.1f} {1:0.1f}'.format(temperature, humidity))
+
+if humidity is not None and temperature is not None:
+    print('{0:0.1f} {1:0.1f}'.format(temperature, humidity))
+else:
+    print("")
