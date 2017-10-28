6a65c01cb74d1b5637aad4bc38848c9e4472e695
Piotr Dorobisz <pdorobisz@gmail.com>
2017-10-29 00:21:45 +0100

    reconnect websocket when connection is lost

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,12 +1,31 @@
 $(function () {
     var socketUrl = $("body").data("ws-url");
-    var webSocket = new WebSocket(socketUrl);
-    webSocket.onmessage = function (message) {
-        var data = JSON.parse(message.data);
-        $("#readings").show();
-        $("#no-data-msg").hide();
-        $("#timestamp-value").text(data.timestamp);
-        $("#temperature-value").text(data.temperature);
-        $("#humidity-value").text(data.humidity);
+    var webSocket = null;
+
+    function startWebSocket() {
+        webSocket = new WebSocket(socketUrl);
+        webSocket.onmessage = function (message) {
+            var data = JSON.parse(message.data);
+            $("#readings").removeClass("hidden");
+            $("#no-data-msg").addClass("hidden");
+            $("#timestamp-value").text(data.timestamp);
+            $("#temperature-value").text(data.temperature);
+            $("#humidity-value").text(data.humidity);
+        };
+        webSocket.onopen = function () {
+            $("#connection-error-msg").addClass("hidden");
+        };
+        webSocket.onclose = function () {
+            $("#connection-error-msg").removeClass("hidden");
+        }
     }
+
+    function checkWebSocket() {
+        if (!webSocket || webSocket.readyState == WebSocket.CLOSED) {
+            startWebSocket();
+        }
+    }
+
+    startWebSocket();
+    setInterval(checkWebSocket, 10000);
 });
